97c01a291d29b3ddbeff449718dbb1421f7d20b2
“Andrii-Yuriev” <andrii.yuriev.dev@gmail.com>
2025-04-11 09:53:10 +0300

    feat: add views, urls & templates for new models Church, Pastor, Disciple
    Also update navbar, footer, base templates

--- religious_atlas/urls.py
+++ religious_atlas/urls.py
@@ -8,4 +8,13 @@ urlpatterns = [
          name="religion_list"),
     path("religions/<int:pk>/", views.ReligionDetailView.as_view(),
          name="religion_detail"),
+
+    path("churches/", views.ChurchListView.as_view(), name="church_list"),
+    path("churches/<int:pk>/", views.ChurchDetailView.as_view(),
+         name="church_detail"),
+
+    path("pastors/", views.PastorListView.as_view(), name="pastor_list"),
+    path("pastors/<int:pk>/", views.PastorDetailView.as_view(),
+         name="pastor_detail"),
+    path("disciples/", views.DiscipleListView.as_view(), name="disciple_list"),
 ]
--- religious_atlas/views.py
+++ religious_atlas/views.py
@@ -48,3 +48,125 @@ class ReligionDetailView(DetailView):
         context["related_churches"] = Church.objects.filter(
             religion=religion_object)
         return context
+
+
+class ChurchListView(ListView):
+    model = Church
+    template_name = "religious_atlas/church_list.html"
+    context_object_name = "churches"
+    paginate_by = 1
+
+    def get_queryset(self):
+        queryset = super().get_queryset().select_related("religion")
+        search_query = self.request.GET.get("q")
+        if search_query:
+            queryset = queryset.filter(
+                Q(name__icontains=search_query) |
+                Q(description__icontains=search_query) |
+                Q(religion__name__icontains=search_query)
+            )
+        return queryset
+
+    def context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update(get_common_counts())
+        context["search_query"] = self.request.GET.get("q", "")
+        return context
+
+
+class ChurchDetailView(DetailView):
+    model = Church
+    template_name = "religious_atlas/church_detail.html"
+    context_object_name = "church"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update(get_common_counts())
+        church_object = self.get_object()
+        context["related_pastors"] = church_object.pastors.prefetch_related(
+            "religion").all()
+        return context
+
+
+class PastorListView(ListView):
+    model = Pastor
+    template_name = "religious_atlas/pastor_list.html"
+    context_object_name = "pastors"
+    paginate_by = 1
+
+    def get_queryset(self):
+        queryset = super().get_queryset().select_related("religion")
+        search_query = self.request.GET.get("q")
+        if search_query:
+            queryset = queryset.filter(
+                Q(username__icontains=search_query) |
+                Q(first_name__icontains=search_query) |
+                Q(last_name__icontains=search_query) |
+                Q(email__icontains=search_query) |
+                (Q(religion__isnull=False) & Q(
+                    religion__name__icontains=search_query))
+            )
+        queryset = queryset.filter(is_superuser=False)
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update(get_common_counts())
+        context["search_query"] = self.request.GET.get("q", "")
+        return context
+
+
+class PastorDetailView(DetailView):
+    model = Pastor
+    template_name = "religious_atlas/pastor_detail.html"
+    context_object_name = "pastor"
+
+    def get_queryset(self):
+        return super().get_queryset().filter(is_superuser=False)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update(get_common_counts())
+        pastor_object = self.get_object()
+
+        context[
+            "serving_churches"] = pastor_object.serving_in_churches.prefetch_related(
+            "religion").all()
+        context["mentors"] = pastor_object.mentors.select_related(
+            "church").all()
+        return context
+
+
+class DiscipleListView(ListView):
+    model = Disciple
+    template_name = "religious_atlas/disciple_list.html"
+    context_object_name = "disciples"
+    paginate_by = 1
+
+    def get_queryset(self):
+        queryset = super().get_queryset().select_related('church',
+                                                         'mentor_pastor',
+                                                         'church__religion')
+        search_query = self.request.GET.get("q")
+        if search_query:
+            queryset = queryset.filter(
+                Q(first_name__icontains=search_query) |
+                Q(last_name__icontains=search_query) |
+                Q(email__icontains=search_query) |
+                (Q(church__isnull=False) & Q(
+                    church__name__icontains=search_query)) |
+                (Q(mentor_pastor__isnull=False) & (
+                        Q(mentor_pastor__username__icontains=search_query) |
+                        Q(mentor_pastor__first_name__icontains=search_query) |
+                        Q(mentor_pastor__last_name__icontains=search_query)
+                ))
+            )
+        return queryset.order_by('last_name',
+                                 'first_name')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update(get_common_counts())
+        context['search_query'] = self.request.GET.get('q',
+                                                       '')
+        return context
